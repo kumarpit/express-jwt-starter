e19949accdd7e8bf47ded948ce90cad25919d3bf
kumarpit <kumar.arpit77@yahoo.com>
2022-06-03 12:47:06 -0700

    more missing returns

--- auth.js
+++ auth.js
@@ -6,10 +6,10 @@ dotenv.config();
 export const authenticateAccessToken = (req, res, next) => {
     const authorization = req.headers['authorization'];
     const token = authorization && authorization.split(" ")[1];
-    if (token == null) res.sendStatus(401);
+    if (token == null) return res.sendStatus(401);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) res.sendStatus(403);
+        if (err) return res.sendStatus(403);
         req.user = user;
         next();
     })
@@ -17,7 +17,7 @@ export const authenticateAccessToken = (req, res, next) => {
 
 export const authenticateRefreshToken = (req, res, next) => {
     const refreshToken = req.body.refresh_token;
-    if (refreshToken == null) res.sendStatus(400);
+    if (refreshToken == null) return res.sendStatus(400);
 
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
         if (err) return res.sendStatus(403);

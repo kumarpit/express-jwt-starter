8348c82f538c81d97512a9f7cf67c9678e1136cb
kumarpit <kumar.arpit77@yahoo.com>
2022-06-02 23:30:23 -0700

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import User from "./models/user.model.js";
 import { authenticateAccessToken, authenticateRefreshToken, generateAccessToken, generateRefreshToken } from "./auth.js";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, {
